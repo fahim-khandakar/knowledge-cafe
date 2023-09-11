98da31be5eb5e892440f7dc08ee3f321f5745294
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-09-12 03:47:29 +0600

    added bookmark

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,50 @@
 import PropTypes from "prop-types";
+import { FaBookmark } from "react-icons/fa";
 
-const Blog = (blog) => {
-  console.log(blog);
-  return <div></div>;
+const Blog = ({ blog, handleAddToBookmark }) => {
+  const {
+    cover,
+    title,
+    author,
+    author_img,
+    posted_time,
+    reading_time,
+    hashtags,
+  } = blog;
+  return (
+    <div className="p-5 py-10 bg-slate-100 rounded-md">
+      <img
+        className="rounded-lg"
+        src={cover}
+        alt={`cover picture of the title ${title}`}
+      />
+      <div className="flex justify-between my-8">
+        <div className="flex items-center gap-5">
+          <img className="w-14" src={author_img} alt="" />
+          <div>
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_time}</p>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <span>{reading_time} min read</span>
+          <button onClick={handleAddToBookmark} className="pl-2">
+            <FaBookmark></FaBookmark>
+          </button>
+        </div>
+      </div>
+
+      <h3 className="text-4xl font-bold mb-5">{title}</h3>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href=""> #{hash}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
-const Blogs = () => {
+const Blogs = ({ handleAddToBookmark }) => {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
@@ -13,6 +14,14 @@ const Blogs = () => {
   return (
     <div className="w-2/3">
       <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+
+      {blogs.map((blog) => (
+        <Blog
+          handleAddToBookmark={handleAddToBookmark}
+          key={blog.id}
+          blog={blog}
+        ></Blog>
+      ))}
     </div>
   );
 };
